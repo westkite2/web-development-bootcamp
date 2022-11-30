51757f835f8120805727641ece50631162e411b6
westkite2 <westkite2@gmail.com>
2022-11-30 12:24:10 +0900

    Complete section 22

---
+++ TodoList_V1/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["feed cat", "blink"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    
+    const day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items})
+
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.post("/", function(req, res){
+    const item = req.body.newItem;
+    if(req.body.list == "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);    
+        res.redirect("/");
+    }
+});
+
+app.post("/work", function(req, res){
+    const item=req.body.newItem;
+    workItems.push(item);
+
+    res.redirect("/work");
+})
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
